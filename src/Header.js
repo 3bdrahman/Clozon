84a02be728f98cf4bf7a012d64af531ebb0f16f8
3bdrahman <abdelrahmanyosef07@gmail.com>
2022-07-12 06:03:33 +0200

    Add React router to project structure

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav className='header'>
+        {/* logo  */}
+        <img className='header__logo' src='./imgs/logo.png' alt='logo'></img>
+        {/* Search bar */}
+        
+        {/* sign in , orders, prime buttons */}
+        {/* orders basket */}
+    </nav>
+  )
+}
+
+export default Header
